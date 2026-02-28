482e9096f726a25536a8a1c1dc47a8769a54be26
AsmaelShowky70 <asmaelshoky70@gmail.com>
2026-02-28 23:10:12 +0200

    fix

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -92,11 +92,11 @@ public class UsersController : ControllerBase
         }
 
         // Validate file extension
-        var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" };
+        var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".heic", ".heif", ".webp" };
         var extension = Path.GetExtension(file.FileName).ToLower();
         if (!allowedExtensions.Contains(extension))
         {
-            return BadRequest(new { message = "Invalid file type. Only JPG, PNG, and GIF are allowed." });
+            return BadRequest(new { message = "Invalid file type. Only JPG, PNG, GIF, HEIC/HEIF or WEBP are allowed." });
         }
 
         // Get current user
